549b2e71ccee6580204fb3d6c8413acc213d0f5b
Moldovan Dennis <dennismoldovan32@gmail.com>
2023-03-26 22:39:47 +0300

    home and login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import { useState } from 'react';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Login from './components/Login';
+import Home from './components/Home';
+
 
 function App() {
 
-
-  
   return (
-    <div className="App">
-      
-     
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
+    </Router>
   );
 }
 
