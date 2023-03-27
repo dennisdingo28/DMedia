fdd5cc5413022cf4e60810ee5fe653582e463e7b
Joker 281 <dennismoldovan32@gmail.com>
2023-03-27 21:12:25 +0300

    h screen

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,15 +3,28 @@ import Logo from './Logo'
 
 const Login = () => {
   return (
-    <div className='bg-darkColor min-h-[100vh] text-white'>
-      <div className='navbar flex items-center justify-between'>
-        <div>
-          <Logo/>
+    <div className='h-screen bg-darkColor text-white'>
+      <div className='contentWrapper parent-container flex flex-col h-[100%]'>
+
+        <div className='navbar flex items-center justify-between py-3'>
+          <div className='cursor-pointer'>
+            <Logo/>
+          </div>
+          <div>
+            <i className="bi bi-chevron-down"></i>
+          </div>
         </div>
-        <div>
-          <i className="bi bi-chevron-down"></i>
+
+        <div className='hero'>
+            <h2 className='font-Open text-[1.4em] font-bold'>Log in and start exploring</h2>
         </div>
+
+        <div className='footer'>
+          <p>Footer here</p>
+        </div>
+
       </div>
+      
     </div>
   )
 }
