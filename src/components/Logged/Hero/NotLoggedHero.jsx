665794fde5475f0884cfbecf97bf7b467eead1b7
Moldovan Dennis <dennismoldovan32@gmail.com>
2023-04-06 22:09:26 +0300

    hero not logged

--- src/components/Logged/Hero/NotLoggedHero.jsx
+++ src/components/Logged/Hero/NotLoggedHero.jsx
@@ -6,9 +6,9 @@ const NotLoggedHero = () => {
     <div className=''>
         <div className='parent-container'>
              <h1 className='font-semibold font-Karla capitalize mt-3 text-center text-[1.79em]'>Happy <span className='text-darkBlue'>to</span> See You <span className='text-darkBlue'>You</span> Here</h1>
-             <div className='md:flex md:flex-row-reverse md:items-center md:justify-evenly'>
+             <div className='md:flex md:flex-row-reverse md:items-center md:justify-between lg:justify-evenly'>
                 <div className='imageContainer'>
-                    <img src={globe} alt='globe' className='max-w-[500px] max-h-[500px] w-[100%] h-[100%] object-cover'/>
+                    <img src={globe} alt='globe' className='max-w-[500px] max-h-[500px] w-[100%] h-[100%] object-cover min-w-[330px] min-h-[330px]'/>
                 </div>
                 <div className='max-w-[500px]'>
                     <p className='font-karla font-medium text-[1.3em]'>We provide the <span className='text-darkViolet'>Experimental</span> Mode to our new <span className='text-darkViolet'>users</span> so that they can easily explore and <span className='text-darkViolet'>explore</span> with our features and <span className='text-darkViolet'>registered</span> users.
