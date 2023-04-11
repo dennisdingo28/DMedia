9e3f9a024daf480f74f742cd71bad9ad2a59426f
Moldovan Dennis <dennismoldovan32@gmail.com>
2023-04-11 13:40:40 +0300

    profile page ui

--- src/components/ProfilePage/Profile.jsx
+++ src/components/ProfilePage/Profile.jsx
@@ -14,50 +14,36 @@ const Profile = (props) => {
   return (
     <div className='min-h-screen bg-dark'>
         <div className='profilePageWrapper parent-container text-white'>
-           <div className='profilePageContainer pt-3'>
-                <h1 className='font-bold text-[1.4em] capitalize'>Manage Your Profile</h1>
-                <div className="profilePageBody mt-5 flex flex-col justify-center rounded-sm p-3 bg-[#131212]">
-                    <div className='profileInformations flex flex-col items-center justify-center'>
-                        <img src={user.profileUrl} className='w-[230px] h-[230px] rounded-full xs:w-[300px] xs:h-[300px]' alt='profile'/>
-                        <h2 className='font-medium font-Noto mt-2 text-[1.1em]'>{user.username}</h2>
-                    </div>
-                    <div className='mt-4'>
-                        <div className='flex items-center justify-around'>
-                            <div className='profileLikes flex flex-col items-center'>
-                                <i className="bi bi-people-fill"></i>
-                                <p>15</p>
-                            </div>
-                            <div className='profileLikes flex flex-col items-center'>
-                                <i className="bi bi-star-fill"></i>
-                                <p>{user.totalLikes}</p>
-                            </div>
-                        
-                            <div className='profileLikes flex flex-col items-center'>
-                                <i className="bi bi-hand-thumbs-down-fill"></i>
-                                <p>{user.totalDislikes}</p>
-                            </div>
-
-                            <div className='profileShares flex flex-col items-center'>
-                                <i className="bi bi-share-fill"></i>
-                                <p>{user.totalShares}</p>
-                            </div>
-                        </div>
-                        <div className='profileTimeStampsContainer mt-3'>
-                            <p className='font-thin'><span className='text-darkViolet'>DMedia</span> Member from: {`${day}/${month<=9 ? '0'+month:month}/${year}`}</p>
-                            <p className='font-thin'><span className='text-darkViolet'>Posts</span> number: {user.posts.length}</p>
-                        </div>
-                    </div>
-        
+            
+            <div className='profilePageLeftSide pt-3'>
+                <div className='flex flex-col items-center justify-center'>
+                    <img src={user.profileUrl} className='w-[220px] h-[220px] rounded-full xs:w-[270px] xs:h-[270px]' alt='profile'/>
+                    <h1 className='font-medium font-Open text-[1.3em]'>{user.username}</h1>
                 </div>
-           </div>
-           <div className='friendsContainer'>
-                <p>friends here</p>
-           </div>
-
-           <div className='profilePostsContainer'>
-                posts here
-           </div>
-
+                <div className='profileRatingContainer flex items-center justify-between'>
+                    <div className='closersContainer flex flex-col gap-1 justify-center items-center'>
+                        <i className='bi bi-people-fill'></i>
+                        <p>150</p>
+                    </div>
+                    <div className='starUpContainer flex flex-col gap-1 justify-center items-center'>
+                        <i className='bi bi-star-fill'></i>
+                        <p>{user.totalLikes}</p>
+                    </div>
+                    <div className='dislikeContainer flex flex-col gap-1 justify-center items-center'>
+                        <i className='bi bi-hand-thumbs-down-fill'></i>
+                        <p>{user.totalDislikes}</p>
+                    </div>
+                    <div className='totalPostsContainer flex flex-col gap-1 justify-center items-center'>
+                        <i className='bi bi-chat-left-text-fill'></i>
+                        <p>{user.posts.length}</p>
+                    </div>
+                    <div className='shareContainer flex flex-col gap-1 justify-center items-center'>
+                        <i className='bi bi-share-fill'></i>
+                        <p>{user.totalShares}</p>
+                    </div>
+                </div>
+            </div>
+        
         </div>
     </div>
   )
